b29878ef32e9b2c54920e96837ef28a7f7fbfcb5
Bootske0 <bootske9@gmail.com>
2024-10-14 22:05:38 +1000

    #2 Переделана маршрутизация (вместо вызова шаблона вызов метода контроллера)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\YourController;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/your-route', [YourController::class, 'yourMethod']);
