4601fff3dffded8735663ab00b4855e767161dd5
crucifyapril <sayaf3@yahoo.com>
2024-10-17 18:20:19 +0300

    #4 правки по миграции товаров

--- database/migrations/2024_10_17_091425_create_products_table.php
+++ database/migrations/2024_10_17_091425_create_products_table.php
@@ -12,13 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id()->primary()->autoIncrement(); // Уникальный идентификатор
-            $table->string('name'); // Наименование
-            $table->decimal('price', 9, 2); // Цена
-            $table->text('description')->nullable(); // Описание
-            $table->integer('quantity')->default(0); // Количество
-            $table->boolean('available_for_sale')->nullable(false); // Доступен ли к продаже
-            $table->timestamps(); // Дата создания и дата последнего изменения
+            $table->id()->primary()->autoIncrement()->comment('Уникальный идентификатор');
+            $table->string('name')->comment('Наименование');
+            $table->integer('price')->comment('Цена');
+            $table->text('description')->nullable()->comment('Описание');
+            $table->integer('quantity')->default(0)->comment('Количество');
+            $table->boolean('available_for_sale')->nullable(false)->comment('Доступен ли к продаже');
         });
     }
 
