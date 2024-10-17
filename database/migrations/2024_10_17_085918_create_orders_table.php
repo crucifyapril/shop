20c8385ff2ab9702891086126adb05ab3ddd8069
crucifyapril <sayaf3@yahoo.com>
2024-10-17 12:09:41 +0300

    #5 создал миграцию заказов

---
+++ database/migrations/2024_10_17_085918_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id(); // Уникальный идентификатор
+            $table->decimal('amount', 9, 2); // Сумма
+            $table->string('status'); // Статус
+            $table->text('description')->nullable(); // Описание
+            $table->timestamps(); // Дата создания и дата последнего изменения
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
