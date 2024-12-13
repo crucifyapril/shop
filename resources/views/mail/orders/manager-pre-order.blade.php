45b80b72e286249d1c2c994ea44b22bf52f9b128
crucifyapril <sayaf3@yahoo.com>
2024-12-13 23:04:54 +0300

    #41 под заказ

--- resources/views/mail/orders/manager-pre-order.blade.php
+++ resources/views/mail/orders/manager-pre-order.blade.php
@@ -1,13 +1,13 @@
 <x-mail::message>
 # Новый подзаказ
-    @dd($data)
-по товару: {{ $data->id }}
+
+по товару: {{ $data->product_id }}
 Пользователь: {{ $data->email }}
 Комментарий: {{ $data->description }}
 
 перейти по ссылке в админ панель:
 
-<x-mail::button :url="url('/admin/resource/order-resource/detail-page?resourceItem=' . $data->id)">
+<x-mail::button :url="url('/admin/resource/order-resource/detail-page?resourceItem=' . $data->product_id)">
     Перейти
 </x-mail::button>
 
