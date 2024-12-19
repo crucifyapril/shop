4a46aeddfbc430fb798423dbadec7199aba84c44
crucifyapril <sayaf3@yahoo.com>
2024-12-19 19:04:44 +0300

    #45 api для контроллера корзины

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ApiCartController;
 use App\Http\Controllers\ApiProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('products', ApiProductController::class)->only('index', 'show');
+Route::apiResource('carts', ApiCartController::class)->only('index', 'store', 'destroy', 'clear');
