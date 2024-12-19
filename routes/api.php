3eec1ad06653d2ee21010399d58feb7b1161b008
crucifyapril <sayaf3@yahoo.com>
2024-12-19 22:19:44 +0300

    #45 апи контроллер для корзины

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('products', ApiProductController::class)->only('index', 'show');
 Route::apiResource('carts', ApiCartController::class)->only('index', 'store', 'destroy', 'clear');
+
+Route::group(['prefix' => 'carts'], function () {
+    Route::delete('/', [ApiCartController::class, 'clear'])->name('api.cart.clear');
+});
