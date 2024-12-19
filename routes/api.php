b781bb256cffb08a344e74b900069fceb7fca958
crucifyapril <sayaf3@yahoo.com>
2024-12-19 14:29:55 +0300

    #44 Api ресурс без лишних роутов

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\ApiProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('products', ApiProductController::class);
+Route::apiResource('products', ApiProductController::class)->only('index', 'show');
