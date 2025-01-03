4dfba7da1b81fd1e6e7e8f05f72e13d07959c296
Aleksandr.tm <aleksandr.tms@yandex.ru>
2025-01-03 23:08:48 +0300

    обработка json ответа для 404

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Enum\Roles;
-use App\Http\Middleware\ForceJsonResponse;
-use App\Http\Middleware\Manager;
 use App\Http\Middleware\Owner;
+use App\Http\Middleware\Manager;
+use App\Http\Middleware\ForceJsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,14 +19,18 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->group('api', [
-            ForceJsonResponse::class,
-        ]);
+        $middleware->api(ForceJsonResponse::class);
         $middleware->alias([
             Roles::MANAGER->value => Manager::class,
             Roles::OWNER->value => Owner::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['success' => false], Response::HTTP_NOT_FOUND);
+            }
+
+            return null;
+        });
     })->create();
